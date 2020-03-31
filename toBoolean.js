5674554e19631d3e5c8b4d2c06293603ce201827
Paul Sammut <paul@pop-os.localdomain>
2020-04-01 09:41:47 +1100

    Added toBoolean.js and uid.js

---
+++ toBoolean.js
@@ -0,0 +1,10 @@
+/*
+ Convert to boolean: "true", "false", "0", "1", true, false, 0, 1  
+*/
+export function toBoolean(value) {
+    var result = false;
+    try {
+        result = !!JSON.parse(String(value).toLowerCase());
+    } catch (e) { }
+    return result;
+}
