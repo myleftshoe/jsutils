845047f4f843f0c766c05881a3e5c58879d15287
Paul Sammut <paul@pop-os.localdomain>
2020-04-03 19:30:39 +1100

    added index.js\

--- toBoolean.js
+++ toBoolean.js
@@ -1,7 +1,7 @@
 /*
  Convert to boolean: "true", "false", "0", "1", true, false, 0, 1  
 */
-export function toBoolean(value) {
+export default function toBoolean(value) {
     var result = false;
     try {
         result = !!JSON.parse(String(value).toLowerCase());
